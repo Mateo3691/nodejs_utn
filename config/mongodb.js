31c90e46e2f433294704867f9644e356a208f63b
mateo.attis <mateo.attis@comafi.com.ar>
2025-05-13 09:25:31 -0300

    se quito mi clave del codigo para que el profesor pueda armas su propia base de datos

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mateojoshua:hrK4dCnmyfcIddWk@test-utn.5ekxvq1.mongodb.net/').then(() => {
+mongoose.connect('clave').then(() => {
   console.log('Conexión a MongoDB exitosa');
 }).catch((error) => {
   console.error('Error de conexión a MongoDB:', error);
